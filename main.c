9fda8caa5e982ca5327c6b5ac470037813d45cc6
Nelson Luiz Serpa de Oliveira <USUARIOS+9793502@6-306-26.grad.usricmc.icmc.usp.br>
2019-09-24 23:19:22 -0300

    still not working but better

--- main.c
+++ main.c
@@ -10,27 +10,38 @@ int main (){
     for(int i = 0; i < image->y; i++){
         position->pointed = image->matrix[i];
         position->start_pos = 0;
-        position->counter = 0; 
-        printf("p/ i = %d temos %d\n",i, *(position->pointed));
+        position->counter = 0;
 
-        int contador = 0;
-        while(((position->start_pos) + (position->counter)) < ((image->x) - 1)){
-            if(image->matrix[i][position->counter + position->start_pos + 1] == *(position->pointed)){
-                printf("Tenho um %d em matrix[%d][%d] igual a %d e na posição posterior em  matrix[%d][%d]\n",*(position->pointed), i, position->counter + position->start_pos,image->matrix[i][position->counter + position->start_pos + 1], i, position->counter + position->start_pos + 1);
+        //printf("Entrei em um i novo. Estou em matrix[%d][%d] position->start_pos = %d conteudo de Position->pointed = %d, position->counter= %d\n",i, ((position->start_pos) + (position->counter)) ,position->start_pos, *(position->pointed), position->counter );
+
+        //printf("p/ i = %d temos %d\n",i, *(position->pointed));
+
+        while(((position->start_pos) + (position->counter)) < ((image->x))){
+            //if(i == 1) printf("Dentro do While, position aponta para matrix[%d][%d] = , e o proximo eh matrix[%d][%d] = %d\n",i,((position->start_pos)+position->counter),i,position->counter + position->start_pos + 1,image->matrix[i][position->counter + position->start_pos + 1]);
+
+            if((image->matrix[i][position->counter + position->start_pos + 1] == *(position->pointed))){
+                //printf("Counter = %d. Tenho um %d em matrix[%d][%d] igual a %d e na posição posterior em  matrix[%d][%d]\n",position->counter,*(position->pointed), i, position->counter + position->start_pos,image->matrix[i][position->counter + position->start_pos + 1], i, position->counter + position->start_pos + 1);
                 position->counter++;
+
+                if(position->counter +1 == image->x){
+                   printf("@ %d %d ", *(position->pointed),position->counter+1);
+                }
             }
 
             else{
                 if(position->counter>3){
-                    printf("Tenho %d %d em seguidos a partir de matrix[%d][%d]", position->counter, *(position->pointed), i, position->start_pos);
+                    printf("@ %d %d ", *(position->pointed),position->counter);
+                    //printf("Tenho %d %d em seguidos a partir de matrix[%d][%d]\n", position->counter, *(position->pointed), i, position->start_pos);
                 }
+                position->counter++;
                 position->pointed  = position->pointed + position->counter; 
                 position->start_pos = position->start_pos + position->counter;
             }
             
         } 
-        printf("Saí do while. position->start_pos = %d conteudo de Position->pointed = %d, position->counter= %d",position->start_pos, *(position->pointed), position->counter );
-        
+        printf("\n");
+        //printf("Saí do while. Estou em matrix[%d][%d] position->start_pos = %d conteudo de Position->pointed = %d, position->counter= %d\n",i, ((position->start_pos) + (position->counter)) ,position->start_pos, *(position->pointed), position->counter );
+
     }
     
 }
